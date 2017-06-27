e415389e1fd4eb5397e5baadeb599f14ca7f7deb
Alex Monteiro <alexwestrj@gmail.com>
2017-06-27 01:58:46 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -25,7 +25,7 @@ Enemy.prototype.update = function(dt) {
     }
 
     // Check for collision with enemies or barrier-walls
-    checkCollision(this);
+    //checkCollision(this);
 };
 
 // Draw the enemy on the screen, required method for game
@@ -57,8 +57,6 @@ Player.prototype.update = function() {
 // Display score, gamelevel
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    displayScoreLevel(score, gameLevel);
-
 };
 
 Player.prototype.handleInput = function(keyPress) {
@@ -75,73 +73,6 @@ Player.prototype.handleInput = function(keyPress) {
         player.y += player.speed - 20;
     }
     console.log('keyPress is: ' + keyPress);
-};
-
-// Function to display player's score
-var displayScoreLevel = function(aScore, aLevel) {
-    var canvas = document.getElementsByTagName('canvas');
-    var firstCanvasTag = canvas[0];
-
-    // Add Player score and level to a div element created
-    scoreLevelDiv.innerHTML = 'Score: ' + aScore +
-        ' / ' + 'Level: ' + aLevel;
-    document.body.insertBefore(scoreLevelDiv, firstCanvasTag[0]);
-};
-
-var checkCollision = function(anEnemy) {
-    // Check for collision between enemy and player
-    if (
-        player.y + 131 >= anEnemy.y + 90 &&
-        player.x + 25 <= anEnemy.x + 88 &&
-        player.y + 73 <= anEnemy.y + 135 &&
-        player.x + 76 >= anEnemy.x + 11) {
-        console.log('I know you can do it! Try again.');
-        player.x = 202.5;
-        player.y = 383;
-    }
-
-    // Check for Player reaching top of canvas and winning the game
-    // If Player wins, add 1 to the score and level
-    // Pass score as an argument to the increaseDifficulty function
-    if (player.y + 63 <= 0) {
-        player.x = 202.5;
-        player.y = 383;
-        console.log('Congratulations! Good luck on next level!');
-
-        ctx.fillStyle = 'white';
-        ctx.fillRect(0, 0, 505, 171);
-
-        score += 1;
-        gameLevel += 1;
-        console.log('Current score: ' + score + ', Current level: ' + gameLevel);
-        increaseDifficulty(score);
-
-    }
-
-    // Check if Player runs into left, bottom, or right canvas walls
-    // Prevent Player from moving beyond canvas wall boundaries
-    if (player.y > 383) {
-        player.y = 383;
-    }
-    if (player.x > 402.5) {
-        player.x = 402.5;
-    }
-    if (player.x < 2.5) {
-        player.x = 2.5;
-    }
-};
-
-// Increase number of enemies on screen based on player's score
-var increaseDifficulty = function(numEnemies) {
-    // Remove all previous enemies on canvas
-    allEnemies.length = 0;
-
-    // load new set of enemies
-    for (var i = 0; i <= numEnemies; i++) {
-        var enemy = new Enemy(0, Math.random() * 184 + 50, Math.random() * 256);
-
-        allEnemies.push(enemy);
-    }
 };
 
 // Now instantiate your objects.
